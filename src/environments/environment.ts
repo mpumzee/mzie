be3f2f5ef6f4eee63fee696478cfe882784c64df
Mpumzee <mpumzcode@gmail.com>
2023-10-03 02:08:53 +0200

    mzi

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'mzie-c8c50',
+    appId: '1:176471133849:web:20ccf27e990e0a40dff208',
+    storageBucket: 'mzie-c8c50.appspot.com',
+    apiKey: 'AIzaSyCRmn4biOdDGQvbBkZgZRheSQr_pMDQpsg',
+    authDomain: 'mzie-c8c50.firebaseapp.com',
+    messagingSenderId: '176471133849',
+  },
+};
